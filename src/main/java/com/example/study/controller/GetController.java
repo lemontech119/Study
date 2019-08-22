7954f718c4aea94dba158fa6c0ba168ef63bba96
lemontech119 <lemontech119@gmail.com>
2019-08-22 13:35:05 +0900

    왜 안될까 ㅠㅠ

---
+++ src/main/java/com/example/study/controller/GetController.java
@@ -0,0 +1,28 @@
+package com.example.study.controller;
+
+
+import com.example.study.model.SearchParam;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class GetController {
+
+    @GetMapping("/test")
+    public String test(){
+        return "get test";
+    }
+
+    @GetMapping("/getMultiParameter")
+    public SearchParam getMultiParameter(SearchParam searchParam){
+        System.out.println(searchParam.getAccount());
+        System.out.println(searchParam.getEmail());
+        System.out.println(searchParam.getPage());
+
+
+        return searchParam;
+    }
+
+}
